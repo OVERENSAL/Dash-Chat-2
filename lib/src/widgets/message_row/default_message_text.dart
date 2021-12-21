5ac4438fb19447503d2a63ae5846a5530951476f
Nickolay Dudin <42511187+OVERENSAL@users.noreply.github.com>
2021-12-21 18:02:06 +0300

    small design fix

--- lib/src/widgets/message_row/default_message_text.dart
+++ lib/src/widgets/message_row/default_message_text.dart
@@ -35,27 +35,24 @@ class DefaultMessageText extends StatelessWidget {
                 : (messageOptions.textColor ?? Colors.black),
           ),
         ),
+        const SizedBox(height: 5),
         Row(
           mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             if (messageOptions.showTime)
               messageOptions.messageTimeBuilder != null
                   ? messageOptions.messageTimeBuilder!(message, isOwnMessage)
-                  : Padding(
-                      padding: const EdgeInsets.only(top: 5),
-                      child: Text(
-                        (messageOptions.timeFormat ?? intl.DateFormat('HH:mm'))
-                            .format(message.createdAt),
-                        style: TextStyle(
-                          color: isOwnMessage
-                              ? (messageOptions.currentUserTextColor ??
-                                  Colors.white70)
-                              : (messageOptions.textColor ?? Colors.black54),
-                          fontSize: 10,
-                        ),
-                      ),
-                    ),
+                  : Text(
+                (messageOptions.timeFormat ?? intl.DateFormat('HH:mm'))
+                    .format(message.createdAt),
+                style: TextStyle(
+                  color: isOwnMessage
+                      ? (messageOptions.currentUserTextColor ??
+                      Colors.white70)
+                      : (messageOptions.textColor ?? Colors.black54),
+                  fontSize: 10,
+                ),
+              ),
             if (isOwnMessage && messageOptions.showReadStatus)
               Padding(
                 padding: const EdgeInsets.only(left: 10),

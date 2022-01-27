d6679d302ca50b6caf2010296910e73350dde560
Nickolay Dudin <42511187+OVERENSAL@users.noreply.github.com>
2022-01-27 11:53:01 +0300

    fixed cansend

--- lib/src/widgets/input_toolbar/input_toolbar.dart
+++ lib/src/widgets/input_toolbar/input_toolbar.dart
@@ -103,7 +103,7 @@ class _InputToolbarState extends State<InputToolbar> {
   }
 
   void _sendMessage() {
-    if (textController.text.isNotEmpty || !widget.canSend) {
+    if (textController.text.trim().isNotEmpty || widget.canSend) {
       final ChatMessage message = ChatMessage(
         text: textController.text,
         user: widget.currentUser,
